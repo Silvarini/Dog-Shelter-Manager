347c3ed4a97b814f1cb1cfb4cb6f2021188e8f24
Bernardo-50039481 <bmsilva@protonmail.com>
2019-10-10 11:47:42 +0100

    Added Controller AdoptionConfirmation, Update Mockups

--- src/pt/iade/dsm/controllers/AdoptionConfirmationController.java
+++ src/pt/iade/dsm/controllers/AdoptionConfirmationController.java
@@ -1,11 +1,14 @@
 package pt.iade.dsm.controllers;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
 
 public class AdoptionConfirmationController {
-	ChoiceBox<String> cb = new ChoiceBox<String>(FXCollections.observableArrayList(
-		    "First", "Second", "Third")
-		);
-}
+   
+ 
+    }
+
 	
